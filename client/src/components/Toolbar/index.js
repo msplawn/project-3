fba04ca8c83938a8dc2e6b9ce2fa6e31074d61d0
Jessica Helft <jessicahelft@gmail.com>
2020-07-28 13:38:44 -0500

    setting up material UI

---
+++ client/src/components/Toolbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Box, Grid, Button, Select} from "@material-ui/core";
+import { StopIcon, PlayArrowIcon} from '@material-ui/icons';
+
+
+
+const Toolbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Toolbar;
