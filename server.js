cddb28729e6f29e1003aaee3fc7a7fd23e8feb68
msplawn <mmsplawn@gmail.com>
2021-12-28 21:34:15 -0600

    deploying

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ client.connect(err => {
 
 // Connect to the MongoDB
 mongoose.connect(
-  process.env.client || "mongodb://localhost/sounds",
+  process.env.MONGODB_URI || "mongodb://localhost/sounds_db",
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 
